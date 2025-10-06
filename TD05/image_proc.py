20c3aa19edd8504a2fb8e4627f6bef7a49d4aaca
PLLima <74931964+PLLima@users.noreply.github.com>
2025-10-06 11:26:04 +0200

    feat: add find_corrections and centile functions

--- TD05/image_proc.py
+++ TD05/image_proc.py
@@ -9,8 +9,6 @@ img = img.astype(int)
  
 #Remove the transparency component
 img = img[:,:,0:3]
-plt.imshow(img, norm=None)
-plt.show()
 
 def grayscale(img):
     return (img[:,:,0] * 0.3  + \
@@ -18,12 +16,41 @@ def grayscale(img):
             img[:,:,2] * 0.11).astype(int)
 
 gray_img = grayscale(img)
-plt.imshow(gray_img, cmap="gray")
-plt.show()
 
 def histogram(gray_img):
     hist = np.zeros(256)
     np.add.at(hist, gray_img, 1)
     return hist / np.sum(hist)
 
-print(histogram(gray_img))
+def plot_intensity(gray_img):
+    hist = histogram(gray_img)
+    x = np.arange(len(hist))
+
+    fig, (img_plt, hist_plt) = plt.subplots(1, 2, figsize=(15, 5))
+    img_plt.set_title("Image")
+    img_plt.imshow(gray_img, cmap="gray")
+
+    hist_plt.plot(x, hist)
+    hist_plt.fill_between(x, hist)
+    hist_plt.set_xlabel("Intensity")
+    hist_plt.set_ylabel("Percentage")
+    hist_plt.set_title("Image Histogram")
+    plt.show(block=True)
+
+plot_intensity(gray_img)
+
+def centile(gray_img):
+    return np.percentile(gray_img, 2), np.percentile(gray_img, 98)
+
+def find_corrections(gray_img):
+    p2, p98 = centile(gray_img)
+
+    contrast_vector = np.array([[0], [255]])
+    centiles_vector = np.array([[p2, 1], [p98, 1]])
+    corrections = np.linalg.solve(centiles_vector, contrast_vector)
+    return corrections[0], corrections[1]
+
+def auto_contrast(gray_img):
+    img = np.zeros_like(gray_img)
+    contrast_correction, intensity_contrast = find_corrections(gray_img)
+
