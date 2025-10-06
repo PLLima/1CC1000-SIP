28204493888137d6e9b1e7d4df905511248012f7
PLLima <74931964+PLLima@users.noreply.github.com>
2025-10-06 10:06:17 +0200

    feat: add grayscale

--- TD05/image_proc.py
+++ TD05/image_proc.py
@@ -4,10 +4,19 @@ import numpy as np
  
 img = plt.imread("raccoon.png")
 #Convert to int
-img = img * 255  
+img = img * 255
 img = img.astype(int)
  
 #Remove the transparency component
 img = img[:,:,0:3]
 plt.imshow(img, norm=None)
+plt.show()
+
+def grayscale(img):
+    return (img[:,:,0] * 0.3  + \
+            img[:,:,1] * 0.59 + \
+            img[:,:,2] * 0.11).astype(int)
+
+img_gray = grayscale(img)
+plt.imshow(img_gray, cmap="gray")
 plt.show()
