180879a1e895672519da3b54a3d321b27d0e1caf
PLLima <74931964+PLLima@users.noreply.github.com>
2025-10-13 10:23:46 +0200

    chore: connect to database

--- TD07-09/src/backend.py
+++ TD07-09/src/backend.py
@@ -1,1 +1,9 @@
 import sqlite3
+
+connexion = sqlite3.connect('./data/database')
+cursor = connexion.cursor()
+
+# print(cursor.execute('''
+#                      SELECT *
+#                      FROM students
+#                      '''))
